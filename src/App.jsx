da65063764abd37549f89ac8fe0e245d7d576185
Hariprasath <truebillionhari@gmail.com>
2024-01-04 20:00:00 +0530

    gj

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Carousel from "./components/Carousel";
+import React from "react";
+function App() {
+  return (
+    <div className="App">
+      <Carousel />
+    </div>
+  );
+}
+
+export default App;
